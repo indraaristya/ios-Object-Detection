7f229c608cb70678f627b0cbe29adaf269d87fe9
indraaristya <indraaristya98@gmail.com>
2018-07-16 16:08:35 +0700

    first commit

---
+++ ObjectDetect/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  ObjectDetect
+//
+//  Created by Indra Aristya on 7/16/18.
+//  Copyright © 2018 Indra Aristya. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var appLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var classificationLabel: UITextView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let imageUsed = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = imageUsed
+            guard let _ciimage = CIImage(image: imageUsed) else {
+                fatalError("Couldn't Conver into CIImage")
+            }
+            detect(image: _ciimage)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Failed to load MLModel")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let result = request.results as? [VNClassificationObservation] else {
+                fatalError("Failed to process Image")
+            }
+            self.navigationItem.title = "Object Detection"
+            self.classificationLabel.text = result.first?.identifier
+            self.appLabel.isHidden = true
+            print(result)
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+    
+
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+}
+
